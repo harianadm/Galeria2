822fb179ff2430a9e3f86b371b571bc351271647
20211tiimi0184 <hariandami@gmail.com>
2023-05-03 09:12:21 -0300

    Galeria 2

--- app/src/main/java/radaelli/chagas/adami/harian/galeria2/MainActivity.java
+++ app/src/main/java/radaelli/chagas/adami/harian/galeria2/MainActivity.java
@@ -1,8 +1,11 @@
 package radaelli.chagas.adami.harian.galeria2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setando a minha toolbar como a toolbar padrao da activity
+        Toolbar toolbar = findViewById(R.id.tbMain);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.opCamera:
+                dispatchTakePictureIntent();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
